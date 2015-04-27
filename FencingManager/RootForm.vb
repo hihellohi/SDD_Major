43404aa4578f7412eba81470cc334359a0dc4f11
he-lium <dominiche2010@gmail.com>
2015-04-27 17:34:03 +1000

    root screen

---
+++ FencingManager/RootForm.vb
@@ -0,0 +1,12 @@
+﻿Public Class RootForm
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If (Screen.PrimaryScreen.Bounds.Width > 1366 And Screen.PrimaryScreen.Bounds.Height > 768) Then
+            Me.FormBorderStyle = Windows.Forms.FormBorderStyle.FixedSingle
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+End Class
