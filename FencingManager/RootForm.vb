bbf2281cb18f9902064892195072fd2cb67017d4
Kevin Ni <hihellohi610@gmail.com>
2015-05-22 22:11:00 +1000

    fix rootform designer not opening
    just follow the damn instructions

--- FencingManager/RootForm.vb
+++ FencingManager/RootForm.vb
@@ -1,6 +1,78 @@
 ﻿Imports System.Data.OleDb
 Imports System.Runtime.InteropServices
 
+
+
+Public Class RootForm
+    Const STUDENT_PROFILE = 0
+    Const GEAR_LOANING = 3
+    Dim formGearLoaning As New GearLoaning()
+    Dim formStudentProfile As New StudentProfilesForm()
+    Dim formAttendance As New AttendanceForm()
+    Dim topform = formStudentProfile
+    Dim intform = STUDENT_PROFILE
+    Public Shared connection As New OleDbConnection
+
+    Private WithEvents kbHook As New KeyboardHook
+
+    Private Sub kbHook_KeyDown(ByVal Key As System.Windows.Forms.Keys) Handles kbHook.KeyDown
+        If intform = GEAR_LOANING Then
+            formGearLoaning.kbhook(Key)
+        End If
+    End Sub
+
+    Private Sub RootForm_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        connection.Close()
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        connection = New OleDbConnection(My.Settings.FencingConnectionString)
+        connection.Open()
+
+        If (Screen.PrimaryScreen.Bounds.Width > 1366 And Screen.PrimaryScreen.Bounds.Height > 768) Then
+            Me.FormBorderStyle = Windows.Forms.FormBorderStyle.FixedSingle
+        End If
+
+        'Load Forms
+
+        formAttendance.TopLevel = False
+        Me.Panel1.Controls.Add(formAttendance)
+
+        formStudentProfile.TopLevel = False
+        Me.Panel1.Controls.Add(formStudentProfile)
+
+        formGearLoaning.TopLevel = False
+        Panel1.Controls.Add(formGearLoaning)
+
+        ' Show Student Profile on Load 
+        'should totes change to login later
+        formStudentProfile.Show()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub btnStudentProf_Click(sender As Object, e As EventArgs) Handles btnStudentProf.Click
+        topform.hide()
+        topform = formStudentProfile
+        intform = STUDENT_PROFILE
+        topform.show()
+    End Sub
+
+    Private Sub btnAttendance_Click(sender As Object, e As EventArgs) Handles btnAttendance.Click
+
+    End Sub
+
+    Private Sub btnGearLoan_Click(sender As Object, e As EventArgs) Handles btnGearLoan.Click
+        topform.hide()
+        topform = formGearLoaning
+        intform = GEAR_LOANING
+        topform.show()
+    End Sub
+End Class
+
 Public Class KeyboardHook
 
     <DllImport("User32.dll", CharSet:=CharSet.Auto, CallingConvention:=CallingConvention.StdCall)> _
@@ -73,73 +145,3 @@ Public Class KeyboardHook
     End Sub
 End Class
 
-
-Public Class RootForm
-    Const STUDENT_PROFILE = 0
-    Const GEAR_LOANING = 3
-    Dim formGearLoaning As New GearLoaning()
-    Dim formStudentProfile As New StudentProfilesForm()
-    Dim formAttendance As New AttendanceForm()
-    Dim topform = formStudentProfile
-    Dim intform = STUDENT_PROFILE
-    Public Shared connection As New OleDbConnection
-
-    Private WithEvents kbHook As New KeyboardHook
-
-    Private Sub kbHook_KeyDown(ByVal Key As System.Windows.Forms.Keys) Handles kbHook.KeyDown
-        If intform = GEAR_LOANING Then
-            formGearLoaning.kbhook(Key)
-        End If
-    End Sub
-
-    Private Sub RootForm_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
-        connection.Close()
-    End Sub
-
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        connection = New OleDbConnection(My.Settings.FencingConnectionString)
-        connection.Open()
-
-        If (Screen.PrimaryScreen.Bounds.Width > 1366 And Screen.PrimaryScreen.Bounds.Height > 768) Then
-            Me.FormBorderStyle = Windows.Forms.FormBorderStyle.FixedSingle
-        End If
-
-        'Load Forms
-
-        formAttendance.TopLevel = False
-        Me.Panel1.Controls.Add(formAttendance)
-
-        formStudentProfile.TopLevel = False
-        Me.Panel1.Controls.Add(formStudentProfile)
-
-        formGearLoaning.TopLevel = False
-        Panel1.Controls.Add(formGearLoaning)
-
-        ' Show Student Profile on Load 
-        'should totes change to login later
-        formStudentProfile.Show()
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Me.WindowState = FormWindowState.Minimized
-    End Sub
-
-    Private Sub btnStudentProf_Click(sender As Object, e As EventArgs) Handles btnStudentProf.Click
-        topform.hide()
-        topform = formStudentProfile
-        intform = STUDENT_PROFILE
-        topform.show()
-    End Sub
-
-    Private Sub btnAttendance_Click(sender As Object, e As EventArgs) Handles btnAttendance.Click
-
-    End Sub
-
-    Private Sub btnGearLoan_Click(sender As Object, e As EventArgs) Handles btnGearLoan.Click
-        topform.hide()
-        topform = formGearLoaning
-        intform = GEAR_LOANING
-        topform.show()
-    End Sub
-End Class
