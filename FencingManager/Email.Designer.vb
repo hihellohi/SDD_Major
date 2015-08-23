dde3dc82867fb0d4f42bd92a53238f3d09a615c8
Kevin Ni <hihellohi610@gmail.com>
2015-08-23 12:18:34 +1000

    improved email

--- FencingManager/Email.Designer.vb
+++ FencingManager/Email.Designer.vb
@@ -22,42 +22,25 @@ Partial Class Email
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnboth = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(112, 173)
-        Me.Button2.Margin = New System.Windows.Forms.Padding(2)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(92, 19)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Send general"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
-        'Button3
-        '
-        Me.Button3.Location = New System.Drawing.Point(112, 206)
-        Me.Button3.Margin = New System.Windows.Forms.Padding(2)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(92, 19)
-        Me.Button3.TabIndex = 2
-        Me.Button3.Text = "Send Both"
-        Me.Button3.UseVisualStyleBackColor = True
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(95, 502)
+        Me.TextBox1.Location = New System.Drawing.Point(6, 32)
         Me.TextBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(287, 108)
+        Me.TextBox1.Size = New System.Drawing.Size(531, 108)
         Me.TextBox1.TabIndex = 3
         '
         'Panel1
@@ -86,22 +69,63 @@ Partial Class Email
         '
         Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button4.Location = New System.Drawing.Point(365, 161)
+        Me.Button4.Location = New System.Drawing.Point(82, 213)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(239, 31)
         Me.Button4.TabIndex = 16
         Me.Button4.Text = "Send Overdue Notices"
         Me.Button4.UseVisualStyleBackColor = True
         '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(297, 274)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(239, 31)
+        Me.Button1.TabIndex = 17
+        Me.Button1.Text = "Send General Email"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnboth
+        '
+        Me.btnboth.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnboth.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnboth.Location = New System.Drawing.Point(82, 325)
+        Me.btnboth.Name = "btnboth"
+        Me.btnboth.Size = New System.Drawing.Size(239, 31)
+        Me.btnboth.TabIndex = 18
+        Me.btnboth.Text = "Send Both"
+        Me.btnboth.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(571, 152)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(542, 311)
+        Me.GroupBox1.TabIndex = 19
+        Me.GroupBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 17)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 18
+        Me.Label2.Text = "Label2"
+        '
         'Email
         '
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
         Me.ClientSize = New System.Drawing.Size(1256, 768)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnboth)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Panel1)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Email"
@@ -109,14 +133,17 @@ Partial Class Email
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Button2 As System.Windows.Forms.Button
-    Friend WithEvents Button3 As System.Windows.Forms.Button
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents Panel1 As System.Windows.Forms.Panel
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents btnboth As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
 End Class
--- FencingManager/Email.vb
+++ FencingManager/Email.vb
@@ -15,20 +15,20 @@ Public Class Email
     Private Sub Button1_Click() Handles Button4.Click
         Dim hashtable As New Hashtable()
         Dim count As Integer = 0
-        Dim things(RootForm.GearDataS.Tables("Gear").Rows.Count) As Integer
+        Dim things(RootForm.GearDataS.Tables("Gear").Rows.Count) As List(Of Integer)
+
         Dim tmp As DataRow
         For Each tmp In RootForm.GearDataS.Tables("Gear").Rows
             If tmp("StudentLoaned") <> 0 Then
 
                 Dim due As New Date(tmp("DueYear"), tmp("DueMonth"), tmp("DueDay"))
                 If due < Date.Today Then
-                    If hashtable.Contains(tmp("StudentLoaned")) Then
-                        things(hashtable(tmp("StudentLoaned"))) += 1
-                    Else
+                    If Not hashtable.Contains(tmp("StudentLoaned")) Then
                         hashtable.Add(tmp("StudentLoaned"), count)
                         count += 1
-                        things(hashtable(tmp("StudentLoaned"))) = 1
+                        things(hashtable(tmp("StudentLoaned"))) = New List(Of Integer)
                     End If
+                    things(hashtable(tmp("StudentLoaned"))).Add(tmp("ID") - 1)
                 End If
             End If
         Next
@@ -53,8 +53,15 @@ Public Class Email
                         email.To.Add(tmpb("Email"))
                         email.Subject = "Overdue Notice"
                         email.IsBodyHtml = True
-                        email.Body = "<html>You have loaned " + things(hashtable(tmpa)).ToString() + " items from your fencing organisation that are overdue. Please return immediately</html>"
-
+                        email.Body = "<!DOCTYPE html><html><head><style>table, th, td {border: 1px solid black;}</style></head><body>You have loaned " + things(hashtable(tmpa)).Count.ToString() + " items from your fencing organisation that are overdue. Please return immediately"
+                        email.Body += "<p><table><tr><th>Gear ID</th><th>Gear Type</th><th>Due Date</th></tr>"
+                        Dim iter As Integer
+                        For Each iter In things(hashtable(tmpa))
+                            email.Body += "<tr><td>" + RootForm.GearDataS.Tables("Gear").Rows(iter).Item("GearID").ToString() + "</td><td>" + RootForm.GearDataS.Tables("Gear").Rows(iter).Item("GearType").ToString() + "</td><td>"
+                            email.Body += RootForm.GearDataS.Tables("Gear").Rows(iter).Item("DueDay").ToString() + "/" + RootForm.GearDataS.Tables("Gear").Rows(iter).Item("DueMonth").ToString() + "/" + RootForm.GearDataS.Tables("Gear").Rows(iter).Item("DueYear").ToString() + "</td></tr>"
+                        Next
+                        email.Body += "</table></p><p>This was an automated message. Do not reply to this email</p></body></html>"
+                        'top kek
                         Smtp_Server.Send(email)
                     Catch error_t As Exception
                         MsgBox(error_t.ToString)
@@ -65,4 +72,7 @@ Public Class Email
         Next
     End Sub
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+    End Sub
 End Class
